fe056b0044895c933cf2386fd1e8b3a9a1539290
kristinayu <kristina.yu@pwc.com>
2021-09-27 01:24:31 +0000

    Added vnet module

--- vnet.tf
+++ vnet.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "test" {
-  name     = "my-resources"
-  location = "West Europe"
+  name     = "${var.prefix}-workshop"
+  location = var.location
 }
 
 module "network" {
